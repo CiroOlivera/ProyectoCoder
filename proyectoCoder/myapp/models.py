3ed45b9f5df4e91a4324be9718185824f2c933a2
ciro olivera <ciroman2001@gmail.com>
2024-12-09 02:01:23 -0300

    Base_de_datos_ok

--- proyectoCoder/myapp/models.py
+++ proyectoCoder/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Socio(models.Model):
+    
+    nombre=models.CharField(max_length=50)
+    mail= models.CharField(max_length=50)
+    tel=models.IntegerField()
+    member=models.CharField(max_length=50)
+    pagoOk=models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nombre
+
+class actividad(models.Model):
+    pass
--- proyectoCoder/myapp/views.py
+++ proyectoCoder/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    context = {"mensaje":"Hola Mundo"}
-    return render(request, "myapp/index.html",context) 
+def index(request):   
+    return render(request, "myapp/index.html") 
+def socios(request):   
+    return render(request, "myapp/socios.html") 
